887c265993605c032059f9232d9928fb02a17473
Error1417 <101829080+Error1417@users.noreply.github.com>
2023-04-19 14:02:49 -0700

    Add files via upload
    Version 1

---
+++ Time_Tracker_Time/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Ismael Perez
+ *
+ * Created on March 24, 2023, 4:46 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "Time.h"
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    Time now;
+    
+    now.printTime(); // print function for the time
+    return 0;
+}
+
